a0ef5f9dcc6716cd42cc071c8807e7b9d7c76e5f
Phu Do <dophu1402@gmail.com>
2021-11-24 13:48:01 +0700

    Fix expired token

--- src/components/courses/CourseList.js
+++ src/components/courses/CourseList.js
@@ -11,52 +11,23 @@ function Courses() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [courses, setCourses] = useState([]);
   const { userInfo, updateUser } = useContext(UserContext);
-  
+
   const tokenLocal = JSON.parse(localStorage.getItem("token"))?.jwtToken;
   // Note: the empty deps array [] means
   // this useEffect will run once
   // similar to componentDidMount()
   useEffect(async () => {
-    let res = await axios.get(`${process.env.REACT_APP_API_URL}/courses/`);
-    if (res.status === 200) {
+    try {
+      let res = await axios.get(`${process.env.REACT_APP_API_URL}/courses/`);
+      // if (res.status === 200) {
       setIsLoaded(true);
       setCourses(res.data);
-    } else {
-      console.log(res);
-      //updateUser(false, null);
+    } catch (err) {
+      console.log(err.response);
+      updateUser(false, null);
       setIsLoaded(true);
-      setError(res.error);
+      setError(err);
     }
-
-    /*
-    fetch(`${process.env.REACT_APP_API_URL}/courses/`, {
-      headers: {
-        "authorization": cookies.token,
-      }
-    })
-      .then(res => {
-        if (res.status === 200) {
-          return res.json();
-        }
-        else {
-          return res.text().then(text => { throw new Error(text) })
-        }
-      })
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setCourses(result);
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          console.log(error);
-          setIsLoaded(true);
-          setError(error);
-        }
-      )
-      */
   }, [])
 
   if (error) {
@@ -68,7 +39,7 @@ function Courses() {
       <div>
         {/* <CreateCourseButton setCourses={setCourses} setError={setError} courses={courses}/> */}
         <Grid container spacing="24px" padding="24px">
-          {courses.map((course,index, courses) => (
+          {courses.map((course, index, courses) => (
             <Grid item key={course.id}>
               <CourseCard
                 course={course}
