05800bf7ba68eb435093c089ed8ec1389aac399d
Lundi <49914842+minhluannguyen@users.noreply.github.com>
2021-12-21 00:47:41 +0700

    Edit Cell

--- src/components/courses/studentRecords/StudentGrades.js
+++ src/components/courses/studentRecords/StudentGrades.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
 import ImportStudentButton from "./ImportStudentButton";
 import UploadFullGradeButton from "./UploadFullGrades";
 import { readString, CSVDownloader } from 'react-papaparse';
@@ -7,23 +7,26 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 import { DataGrid } from '@mui/x-data-grid';
 import Box from '@mui/material/Box';
+import { SnackbarContext } from "../../../contexts/SnackbarContext";
+import { Typography } from '@mui/material';
 
 export default function StudentGrades({ gradeStructure }) {
-    const [openPreview, setOpenPreview] = useState(false);
-    const [fileName, setFileName] = useState("");
     const [csvData, setCsvData] = useState([]);
     const [pageSize, setPageSize] = useState(5);
     const [loading, setLoading] = useState(false);
+    const [isSaved, setIsSaved] = useState(true);
     const { id } = useParams();
     const [columnHeaders, setColumnHeaders] = useState([]);
+	const { handleOpenErrorSnack, handleOpenSuccessSnack, handleSetMsgSnack } = useContext(SnackbarContext);
 
     useEffect(async () => {
+        setLoading(true);
         try {
             let res = await axios.get(`${process.env.REACT_APP_API_URL}/courses/${id}/grades`);
             // if (res.status === 200) {
             console.log(res.data);
             setCsvData(res.data);
-            //setIsLoaded(true);
+            setLoading(false);
             //setCourses(res.data);
             let totalPoint = 0;
             gradeStructure.forEach(async (grade) => {
@@ -43,9 +46,8 @@ export default function StudentGrades({ gradeStructure }) {
         
             const totalHeader = {
                 field: 'total',
-                headerName: `total \n (${totalPoint})`,
+                headerName: `Total \n (${totalPoint})`,
                 width: 150,
-                editable: true,
                 type: "number"
             }
         
@@ -57,20 +59,44 @@ export default function StudentGrades({ gradeStructure }) {
             setColumnHeaders(columnsArray);
 
         } catch (err) {
+            setLoading(false);
             console.log(err);
-            if (err.response?.status === 401) {
-            //updateUser(false, null);
-            }
-            //setIsLoaded(true);
-            //setError(err);
+            handleOpenErrorSnack(true);
+            handleSetMsgSnack(err.response.data.message);
         }
     }, [])
+
+    async function onCellChange(params, event, detail) {
+        let newCsvData = csvData;
+        newCsvData[params.id][`${params.field}`] = params.value;
+        setCsvData(newCsvData);
+        setIsSaved(false);
+        //Send to server to save
+        try {
+            await axios.post(`${process.env.REACT_APP_API_URL}/courses/${id}/grades/update-one-row`, { data: csvData[params.id] });
+            setIsSaved(true);
+
+        } catch (err) {
+            //setOpenPreview(false);
+            handleOpenErrorSnack(true);
+            handleSetMsgSnack(err.response.data.message);
+        }
+    }
 
     return (
         <div>
             <Box
                 component="span"
             >
+                <Typography
+                    variant="h5"
+                    component="div"
+                    align="right"
+                    sx={{ color: '#9e9e9e', cursor: 'pointer' }}
+                >
+                    {loading ? "" : (isSaved ? "Saved" : "Saving . . .")}
+                </Typography>
+
                 <DataGrid
                     autoHeight
                     columns={columnHeaders}
@@ -79,6 +105,8 @@ export default function StudentGrades({ gradeStructure }) {
                     onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
                     rowsPerPageOptions={[5, 10, 20, 50, 100]}
                     pagination
+                    loading={loading}
+                    onCellEditCommit={onCellChange}
                     {...csvData}
                 />
             </Box>
